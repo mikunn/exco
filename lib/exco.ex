43392295ba0a203ad8b462b3463aa49808a1acf4
Mika Kunnas <mika.kunnas@viidakko.fi>
2018-01-20 19:09:06 +0200

    use async/await by default and async stream with restricted concurrency

--- lib/exco.ex
+++ lib/exco.ex
@@ -1,31 +1,65 @@
 defmodule Exco do
 
-  def map(enumerable, fun) do
-    enumerate(:map, enumerable, fun)
+  alias Exco.Opts
+
+  @default_options [
+    max_concurrency: :full
+  ]
+
+  def map(enumerable, fun, opts \\ []) do
+    run(:map, enumerable, fun, opts)
   end
 
-  def each(enumerable, fun) do
-    enumerate(:each, enumerable, fun)
+  def each(enumerable, fun, opts \\ []) do
+    run(:each, enumerable, fun, opts)
   end
 
-  def filter(enumerable, fun) do
-    enumerate(:filter, enumerable, fun)
+  def filter(enumerable, fun, opts \\ []) do
+    run(:filter, enumerable, fun, opts)
   end
 
-  defp enumerate(:map, enumerable, fun) do
-    get_stream(enumerable, fun)
+  defp run(operation, enumerable, fun, opts) do
+    opts = Opts.set_defaults(opts, @default_options)
+           |> Opts.normalize(operation, enumerable)
+           |> Enum.into(%{})
+    enumerate(operation, enumerable, fun, opts)
+  end
+
+  defp enumerate(:map, enumerable, fun, %{max_concurrency: conc}) when conc == :full do
+    get_awaited_map(enumerable, fun)
+  end
+
+  defp enumerate(:map, enumerable, fun, %{max_concurrency: conc}) do
+    get_async_stream(enumerable, fun, max_concurrency: conc)
     |> Enum.map(fn {:ok, value} -> value end)
   end
 
-  defp enumerate(:each, enumerable, fun) do
-    get_stream(enumerable, fun)
+  defp enumerate(:each, enumerable, fun, %{max_concurrency: conc}) when conc == :full do
+    get_awaited_map(enumerable, fun)
     |> Enum.each(fn value -> value end)
   end
 
-  defp enumerate(:filter, enumerable, fun) do
-    get_stream(enumerable, fun)
+  defp enumerate(:each, enumerable, fun, %{max_concurrency: conc}) do
+    get_async_stream(enumerable, fun, max_concurrency: conc)
+    |> Enum.each(fn value -> value end)
+  end
+
+  defp enumerate(:filter, enumerable, fun, %{max_concurrency: conc}) when conc == :full do
+    get_awaited_map(enumerable, fun)
     |> Enum.zip(enumerable)
-    |> Enum.filter(fn res -> 
+    |> Enum.filter(fn res ->
+      case res do
+        {true, _} -> true
+        _ -> false
+      end
+    end)
+    |> Enum.map(fn {true, value} -> value end)
+  end
+
+  defp enumerate(:filter, enumerable, fun, %{max_concurrency: conc}) do
+    get_async_stream(enumerable, fun, max_concurrenct: conc)
+    |> Enum.zip(enumerable)
+    |> Enum.filter(fn res ->
       case res do
         {{:ok, true}, _} -> true
         _ -> false
@@ -34,8 +68,15 @@ defmodule Exco do
     |> Enum.map(fn {{:ok, true}, value} -> value end)
   end
 
-  defp get_stream(enumerable, fun) do
-    enumerable |> Task.async_stream(fun)
+  defp get_async_stream(enumerable, fun, options) do
+    enumerable
+    |> Task.async_stream(fun, options)
+  end
+
+  defp get_awaited_map(enumerable, fun) do
+    enumerable
+    |> Enum.map(&(Task.async(fn -> fun.(&1) end)))
+    |> Enum.map(&Task.await/1)
   end
 
 end
