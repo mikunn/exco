10796cdffb8f9e2da2a9e23150358021d8e64b0c
Mika Kunnas <mika.kunnas@gmail.com>
2018-01-23 18:54:36 +0200

    add map linkage tests to exco_test

--- test/exco_test.exs
+++ test/exco_test.exs
@@ -37,6 +37,85 @@ defmodule ExcoTest do
     assert Exco.filter([1, 2, 3], &(&1 * 2 < 5), linkage: :nolink) == [ok: 1, ok: 2]
   end
 
+  test "map: tasks run to completion when caller finishes" do
+    pid = self()
+
+    spawn(fn ->
+      Exco.map([1, 2, 3], fn x ->
+        Process.sleep(50)
+        send(pid, :task_alive)
+        x
+      end)
+
+      send(pid, :result)
+    end)
+
+    Process.sleep(30)
+
+    assert_receive :task_alive
+    assert_receive :result
+  end
+
+  test "map: tasks die when caller dies" do
+    pid = self()
+
+    caller =
+      spawn(fn ->
+        Exco.map([1, 2, 3], fn x ->
+          Process.sleep(50)
+          send(pid, :task_alive)
+          x
+        end)
+      end)
+
+    Process.sleep(30)
+    Process.exit(caller, :kill)
+
+    refute_receive :task_alive
+  end
+
+  test "map with max_concurrency: tasks die when caller dies" do
+    pid = self()
+
+    fun = fn x ->
+      Process.sleep(50)
+      send(pid, :task_alive)
+      x
+    end
+
+    caller =
+      spawn(fn ->
+        Exco.map(
+          [1, 2, 3],
+          fun,
+          max_concurrency: 2
+        )
+      end)
+
+    Process.sleep(30)
+    Process.exit(caller, :kill)
+
+    refute_receive :task_alive
+  end
+
+  test "map: caller dies when a task dies" do
+    pid = self()
+
+    spawn(fn ->
+      spawn_link(fn ->
+        Exco.map([1, 0, 2], fn _ ->
+          Process.sleep(10)
+          Process.exit(self(), :kill)
+        end)
+      end)
+
+      Process.sleep(50)
+      send(pid, :caller_alive)
+    end)
+
+    refute_receive :caller_alive
+  end
+
   defp each_receive_loop(result, counter \\ 1) do
     receive do
       {:value, x, total} when total == counter ->
